3d30ec527097be71aed5345cff5778df753270d4
Victor <43469416+VityaP@users.noreply.github.com>
2021-05-08 18:27:14 +0300

    Add files via upload

--- UnitTestFramework.cpp
+++ UnitTestFramework.cpp
@@ -1,92 +1,94 @@
 #include <iostream>
-#include <cassert>
-#include <string>
-#include <set>
 #include <map>
-#include <vector>
+#include <set>
 #include <sstream>
-#include <utility>
-#include <exception>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-template<class T>
-ostream& operator << (ostream& os, const set<T> &s){
-    os << "{";
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+    os << "[";
     bool first = true;
-    for(const auto &x : s){
-        if( !first ){
+    for (const auto& x : v) {
+        if (!first) {
             os << ", ";
         }
         first = false;
         os << x;
     }
-    os << "}";
-    return os;
+    return os << "]";
 }
 
-template<class K, class V>
-ostream& operator << (ostream& os, const map<K, V> &m){
+template <class T>
+ostream& operator<<(ostream& os, const set<T>& s) {
     os << "{";
     bool first = true;
-    for(const auto &x : m){
-        if( !first ){
+    for (const auto& x : s) {
+        if (!first) {
             os << ", ";
         }
         first = false;
-        os << x.first << ": " << x.second;
+        os << x;
     }
-    os << "}";
-    return os;
+    return os << "}";
+}
+
+template <class K, class V>
+ostream& operator<<(ostream& os, const map<K, V>& m) {
+    os << "{";
+    bool first = true;
+    for (const auto& kv : m) {
+        if (!first) {
+            os << ", ";
+        }
+        first = false;
+        os << kv.first << ": " << kv.second;
+    }
+    return os << "}";
 }
 
 template<class T, class U>
-void myAssert(const T& t, const U& u, const string &hint){
-    if( t != u ){
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+    if (t != u) {
         ostringstream os;
-        os << "Assertion failed: " << t << " != " << u << "Hint: " << hint;
+        os << "Assertion failed: " << t << " != " << u;
+        if (!hint.empty()) {
+            os << " hint: " << hint;
+        }
         throw runtime_error(os.str());
     }
 }
 
-class TestRunner{
+void Assert(bool b, const string& hint) {
+    AssertEqual(b, true, hint);
+}
 
+class TestRunner {
 public:
-    template<class TestFunc>
-    void RunTest(TestFunc func, const string & testName){
-        try{
+    template <class TestFunc>
+    void RunTest(TestFunc func, const string& test_name) {
+        try {
             func();
-            cerr << testName << " OK" << "\n";
-        }
-        catch (runtime_error &e){
-            ++failCount = 0;
-            cerr << testName << " fail: " << e.what() << "\n";
+            cerr << test_name << " OK" << endl;
+        } catch (exception& e) {
+            ++fail_count;
+            cerr << test_name << " fail: " << e.what() << endl;
+        } catch (...) {
+            ++fail_count;
+            cerr << "Unknown exception caught" << endl;
         }
     }
 
-    ~TestRunner(){
-        if( failCount > 0){
-            cerr << failCount << " tests failed. Terminate";
+    ~TestRunner() {
+        if (fail_count > 0) {
+            cerr << fail_count << " unit tests failed. Terminate" << endl;
             exit(1);
         }
     }
 
 private:
-    int failCount = 0;
-
+    int fail_count = 0;
 };
-
-void TestAll(){
-    TestRunner tr;
-    //tr.RunTest(func1, "func1");
-    //tr.RunTest(func2, "func2");
-    //tr.RunTest(func3, "func3");
-    // ...
-}
-
-int main(){
-
-    TestAll();
-
-    return 0;
-}
